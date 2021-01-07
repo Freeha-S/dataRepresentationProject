bb2f04ad854068b3790137b0a27be88267a33338
freeha198 <66973232+freeha198@users.noreply.github.com>
2021-01-07 04:10:37 +0000

    Update ShopDAO.py

--- ShopDAO.py
+++ ShopDAO.py
@@ -7,67 +7,64 @@ import dbconfiguration as cfg
 
 # Class object for Database Access Object (DAO)
 class ShopDAO:
-
+   db=""
    # Create database connection pool
    def initConnectToDB(self):
-      db = mysql.connector.connect(
-         host=cfg.mysql['host'],
-         user=cfg.mysql['username'],
-         password=cfg.mysql['password'],
-         database=cfg.mysql['database'],
-         pool_name='my_connection_pool',
-         pool_size=6
-      )
-      return db
-
-   # Get a connection from the pool
-   def getConnection(self):
-      db = mysql.connector.connect(
-         pool_name='my_connection_pool'
-      )
-      return db
-
+      self.db = mysql.connector.connect(
+      host=cfg.mysql['host'],
+      user=cfg.mysql['username'],
+      password=cfg.mysql['password'],
+      database=cfg.mysql['database'],
+    )
    # Initialise DB connection pool
    def __init__(self):
-      db = self.initConnectToDB()
-      db.close()
+      self.initConnectToDB()
+
+
+   # Check for cnxn, if none make one.
+   def getConnection(self):
+      if not self.db.is_connected():
+         self.initConnectToDB()
+      return self.db.cursor()
+
+
 
    # Create customer, returns customerID of new customer
    def createCustomer(self, customer):
-      db = self.getConnection()
-      cursor = db.cursor()
+      cursor = self.getConnection()
+      #cursor = db.cursor()
       sql = "insert into Customer(FirstName, LastName, Address,Email,PhoneNumber) values (%s,%s,%s,%s,%s)"
       values = [customer[0],customer[1],customer[2],customer[3],customer[4]]
       cursor.execute(sql, values)
-      db.commit()
+      self.db.commit()
       lastRowId = cursor.lastrowid
-      db.close()
+      cursor.close()
       return lastRowId
       # return customerID
 
    # Create Orders, returns OrderId of new Order
    def createOrder(self, order):
-      db = self.getConnection()
-      cursor = db.cursor()
+
+      cursor = self.getConnection()
       sql = "insert into Orders (Product, OrderDate, Category, Customer_Id) values (%s,%s,%s,%s)"
       values = [
-        
+
          order[0],
          order[1],
          order[2],
          order[3]
       ]
       cursor.execute(sql, values)
-      db.commit()
+      self.db.commit()
       lastRowId = cursor.lastrowid
-      db.close()
+      cursor.close()
       return lastRowId
       # return order['orderId']
 
    # Create user, returns userID of new user
    def createUser(self, user):
-      db = self.getConnection()
-      cursor = db.cursor()
+
+      cursor = self.getConnection()
       sql = "insert into Users (Name, Password) values (%s,%s)"
       values = [
          # u['userID'], - auto-increment
@@ -75,16 +72,16 @@ class ShopDAO:
          user['password'],
       ]
       cursor.execute(sql, values)
-      db.commit()
+      self.db.commit()
       lastRowId = cursor.lastrowid
-      db.close()
+      cursor.close()
       return lastRowId
       # return user['userID']
 
    # Return all customers from database
    def getAllCustomer(self):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'select * from Customer'
       cursor.execute(sql)
       results = cursor.fetchall()
@@ -93,13 +90,13 @@ class ShopDAO:
       for result in results:
          resultAsDict = self.convertCustomerToDict(result)
          returnArray.append(resultAsDict)
-      db.close()
+      cursor.close()
       return returnArray
 
    # Return all orders from database
    def getAllOrders(self):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'select * from Orders'
       cursor.execute(sql)
       results = cursor.fetchall()
@@ -108,13 +105,13 @@ class ShopDAO:
       for result in results:
          resultAsDict = self.convertOrderToDict(result)
          returnArray.append(resultAsDict)
-      db.close()
+      cursor.close()
       return returnArray
 
    # Return info on all users
    def getAllUser(self):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'select * from Users'
       cursor.execute(sql)
       results = cursor.fetchall()
@@ -124,61 +121,61 @@ class ShopDAO:
          resultAsDict = self.convertUserToDict(result)
          # print(result)
          returnArray.append(resultAsDict)
-      db.close()
+      cursor.close()
       return returnArray
 
    # Return info on Customer for given customerID
    def findCustomerById(self, customerID):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'select * from Customer where CustomerID = %s'
       values = [customerID]
       cursor.execute(sql, values)
       result = cursor.fetchone()
       customer = self.convertCustomerToDict(result)
-      db.close()
+      cursor.close()
       return customer
 
    # Return orders for given orderID
    def findOrderById(self, orderId):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'select * from Orders where OrderId = %s'
       values = [orderId]
       cursor.execute(sql, values)
       result = cursor.fetchone()
       order= self.convertOrderToDict(result)
-      db.close()
+      cursor.close()
       return order
 
    # Return user for given userID
    def findUserByID(self, userId):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'select * from Users where userID = %s'
       values = [userId]
       cursor.execute(sql, values)
       result = cursor.fetchone()
       user = self.convertUserToDict(result)
-      db.close()
+      cursor.close()
       return user
    #Return user for given user name
    def findUserByUserID(self, name):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'select * from Users where name = %s'
       values = [name]
       cursor.execute(sql, values)
       result = cursor.fetchone()
       user = self.convertUserToDict(result)
-      db.close()
+      cursor.close()
       return user
 
 
    # Return info on all Orders associated with a given customer
    def getAllOrderByCustomer(self, customerid):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'select * from Orders where Customer_Id = %s;'
       values = [customerid]
       cursor.execute(sql, values)
@@ -188,26 +185,26 @@ class ShopDAO:
       for result in results:
          resultAsDict = self.convertOrderToDict(result)
          returnArray.append(resultAsDict)
-      db.close()
+      cursor.close()
       return returnArray
 
    # Update customer for given customerID and returns updated info
    def updateCustomer(self, customer):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       #cursor=conn.cursor()
       sql = "update customer set FirstName= %s, LastName =%s ,Address= %s,Email= %s,PhoneNumber= %s where CustomerID = %s"
-      
+
       values = [customer['FirstName'],customer['LastName'],customer['Address'],customer['Email'],customer['PhoneNumber'],customer['CustomerID']]
       cursor.execute(sql, values)
       db.commit()
-      db.close()
+      cursor.close()
       return customer
 
    # Update order info for given orderid
    def updateOrder(self, order):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = "update Orders set Product = %s, OrderDate = %s, Category = %s, Customer_Id = %s where OrderId = %s"
       values = [
          order['Product'],
@@ -217,14 +214,14 @@ class ShopDAO:
          order['OrderId']
       ]
       cursor.execute(sql, values)
-      db.commit()
-      db.close()
+      self.db.commit()
+      cursor.close()
       return order
 
    # Update user info
    def updateUser(self, user):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = "update users set name = %s, password = %s where userID = %s"
       values = [
          user['name'],
@@ -232,33 +229,33 @@ class ShopDAO:
          user['userID']
       ]
       cursor.execute(sql, values)
-      db.commit()
-      db.close()
+      self.db.commit()
+      cursor.close()
       return user
 
    # Delete customer for given customerId
    def deleteCustomer(self, customerId):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'delete from Customer where CustomerId = %s'
       values = [customerId]
       cursor.execute(sql, values)
-      db.commit()
-      db.close()
+      self.db.commit()
+      cursor.close()
       return {}
 
    # Delete Order for a given orderId
    def deleteOrder(self, orderId):
-      db = self.getConnection()
-      cursor = db.cursor()
+      #db = self.getConnection()
+      cursor = self.getConnection()
       sql = 'delete from Orders where OrderId = %s'
       values = [orderId]
       cursor.execute(sql, values)
-      db.commit()
-      db.close()
+      self.db.commit()
+      cursor.close()
       return {}
 
-   
+
    # convert Customer into Dictionary
    def convertCustomerToDict(self, result):
       colnames = ['CustomerID', 'FirstName', 'LastName', 'Address', 'Email', 'PhoneNumber']
@@ -277,7 +274,7 @@ class ShopDAO:
          for i, colName in enumerate(colnames):
             value = result[i]
             order[colName] = value
-        
+
       return order
 
    # convert user into Dictionary
